dd173df78576f831f55148c5c966f72cda329f94
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-02-25 22:15:30 -0500

    Add market preview

--- pages/market.tsx
+++ pages/market.tsx
@@ -1,21 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Ponies from "../components/Ponies";
+import Market from "../components/Market/Market";
+import React from "react";
 
-const PoniesPage: NextPage = () => {
+const MarketPage: NextPage = () => {
   return (
     <div className="container">
       <Head>
-        <title>Blacksand</title>
+        <title>BlackSand</title>
         <meta name="description" content="Welcome to the BlackSand" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="main">
-        <Ponies />
+        <Market />
       </main>
     </div>
   );
 };
 
-export default PoniesPage;
+export default MarketPage;
