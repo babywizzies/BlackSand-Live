8424f7cb2066c0a6051c619a3549d01b6e7aa68f
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-06-27 09:49:19 -0400

    fix burn page build errors

--- components/Market/LeifDojang.tsx
+++ components/Market/LeifDojang.tsx
@@ -1,52 +1,77 @@
-import React from 'react'
-import styles from '../../styles/css/artist.module.css'
-import Link from 'next/link'
+import React from "react";
+import styles from "../../styles/css/artist.module.css";
+import Link from "next/link";
 
 const LeifDojang = () => {
   return (
     <div className={styles.container}>
-        <header className={styles.header}>
-            <img src="/img/leif.jpg"/>
-            <div className={styles.name}>
-                <h1>Leif Dojang</h1>
-                <span>@LDojangMusic</span>  
-            </div>
-        </header>
-        <p>Sound Designer, Composer & Musician, crafting original pieces for BlackSand Citizens, Racers and the wider FRWC community.</p>
-        <div className={styles.social}>
-            <p>Website: https:/leifdojang.com</p>
-            <p>Twitter: @LDojangMusic</p>
+      <header className={styles.header}>
+        <img src="/img/leif.jpg" />
+        <div className={styles.name}>
+          <h1>Leif Dojang</h1>
+          <span>@LDojangMusic</span>
         </div>
-        <div className={styles.subcontainer}>
+      </header>
+      <p>
+        Sound Designer, Composer & Musician, crafting original pieces for
+        BlackSand Citizens, Racers and the wider FRWC community.
+      </p>
+      <div className={styles.social}>
+        <p>Website: https:/leifdojang.com</p>
+        <p>Twitter: @LDojangMusic</p>
+      </div>
+      <div className={styles.subcontainer}>
         <div className={styles.card}>
-        <iframe width="380" height="215" src="https://www.youtube.com/embed/hSElehoZ3zc?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe
+            width="380"
+            height="215"
+            src="https://www.youtube.com/embed/hSElehoZ3zc?controls=0"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
           <div className={styles.subcard}>
             <h1>Music Piece</h1>
             <p>Short original theme song for your FRWC character of choice.</p>
-            <br/>
+            <br />
             <b>Redeem for</b>
             <div className={styles.button}>
-                <button className={styles.button_9} role="button">250 FLORINS</button>
+              <button className={styles.button_9} role="button">
+                250 FLORINS
+              </button>
             </div>
           </div>
         </div>
-      <Link href="/">
-        <div className={styles.card}>
-        <iframe width="380" height="215" src="https://player.vimeo.com/video/788867872?h=119de51e9b&title=0&byline=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-          <div className={styles.subcard}>
-            <h1>Music + Narrated Lore Piece</h1>
-            <p>Theme song based on your existing lore, that ties music and lore together.</p>
-            <br/>
-            <b>Redeem for</b>
-            <div className={styles.button}>
-                <button className={styles.button_9} role="button">500 FLORINS</button>
+        <Link href="/">
+          <div className={styles.card}>
+            <iframe
+              width="380"
+              height="215"
+              src="https://player.vimeo.com/video/788867872?h=119de51e9b&title=0&byline=0"
+              frameBorder="0"
+              allow="autoplay; fullscreen; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className={styles.subcard}>
+              <h1>Music + Narrated Lore Piece</h1>
+              <p>
+                Theme song based on your existing lore, that ties music and lore
+                together.
+              </p>
+              <br />
+              <b>Redeem for</b>
+              <div className={styles.button}>
+                <button className={styles.button_9} role="button">
+                  500 FLORINS
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-     </div>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeifDojang
+export default LeifDojang;
