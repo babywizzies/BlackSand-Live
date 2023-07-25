ed1953da6ce094a529bee727afcf97240b637057
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-07-25 10:31:37 -0400

    Add limit to user tokens reservoir api call

--- utils/tokenUtils.tsx
+++ utils/tokenUtils.tsx
@@ -1,57 +1,66 @@
 import axios from "axios";
 
 export const getUserTokens = async (address: string) => {
-    const options = { method: 'GET', headers: { accept: '*/*' } };
-    const contractAddress = '0xf486f696B80164B5943191236ECa114f4EfAb2FF';
-    return axios.get('https://api.reservoir.tools/users/' + address + '/tokens/v6?collection=' + contractAddress, options)
-}
+  const options = { method: "GET", headers: { accept: "*/*" } };
+  const contractAddress = "0xf486f696B80164B5943191236ECa114f4EfAb2FF";
+  return axios.get(
+    `https://api.reservoir.tools/users/${address}/tokens/v6?collection=${contractAddress}&limit=200`,
+    options
+  );
+};
 
-export const userOwnsToken = async (tokenId: string, tokensOwned: TokenData[]) => {
-    if (tokensOwned == undefined) {
-        return false;
-    }
-
-    for (let i in tokensOwned) {
-        if (tokensOwned[i].token.tokenId == tokenId) {
-            return true;
-        };
-    }
-
+export const userOwnsToken = async (
+  tokenId: string,
+  tokensOwned: TokenData[]
+) => {
+  if (tokensOwned == undefined) {
     return false;
-}
+  }
 
-export const getPonyName = async (tokenId: string, tokensOwned: TokenData[]) => {
-    for (let i in tokensOwned) {
-        if (tokensOwned[i].token.tokenId == tokenId) {
-            return tokensOwned[i].token.name;
-        };
+  for (let i in tokensOwned) {
+    if (tokensOwned[i].token.tokenId == tokenId) {
+      return true;
     }
-    return undefined;
-}
+  }
+
+  return false;
+};
+
+export const getPonyName = async (
+  tokenId: string,
+  tokensOwned: TokenData[]
+) => {
+  for (let i in tokensOwned) {
+    if (tokensOwned[i].token.tokenId == tokenId) {
+      return tokensOwned[i].token.name;
+    }
+  }
+  return undefined;
+};
 
 export interface TokenData {
-    token: Token;
+  token: Token;
 }
 
 export interface TokenElement {
-    token: Token;
+  token: Token;
 }
 
 export interface Token {
-    contract: string;
-    tokenId: string;
-    kind: string;
-    name: string;
-    image: string;
-    rarityScore: null;
-    rarityRank: null;
-    media: null;
-    collection: Collection;
+  contract: string;
+  tokenId: string;
+  kind: string;
+  name: string;
+  image: string;
+  rarityScore: null;
+  rarityRank: null;
+  media: null;
+  collection: Collection;
 }
 
 export interface Collection {
-    id: string;
-    name: string;
-    imageUrl: string;
-    floorAskPrice: number;
+  id: string;
+  name: string;
+  imageUrl: string;
+  floorAskPrice: number;
 }
