68ec1fd2cf79fa06f15bfac2de98403599eafb97
babywizzies <104694001+babywizzies@users.noreply.github.com>
2023-09-19 12:37:59 -0400

    integrate babies + warriors

--- components/Wardrobe/Wardrobe2.tsx
+++ components/Wardrobe/Wardrobe2.tsx
@@ -13,6 +13,13 @@ enum EquipScreen {
     Confirmation,
     Portal,
 }
+
+const CONTRACT_TO_COLLECTION_MAP: Record<string, string> = {
+    '0x521f9c7505005cfa19a8e5786a9c3c9c9f5e6f42': 'wizards',
+    '0x4b1e130ae84c97b931ffbe91ead6b1da16993d45': 'babies',
+    '0x9690b63eb85467be5267a3603f770589ab12dc95': 'warriors'
+};
+
 const Wardrobe2: React.FC = () => {
     const { address: accountAddress } = useAccount();
     const [selectedCharacter, setSelectedCharacter] = useState<any>(null);
@@ -23,25 +30,33 @@ const Wardrobe2: React.FC = () => {
         console.log(characterData);
     }, [characterData]);
     const handleCharacterSelect = (character: { characterId: string; contract: string }) => {
+        
         setSelectedCharacter(character);
     };
-    const handleEquipItem = (itemId: string) => {
+    const handleEquipItem = (itemId: string, traitType: string) => {
         if (characterData && characterData.attributes) {
-            // Find the 'prop' trait
-            const propTrait = characterData.attributes.find((attr) => attr.trait_type.toLowerCase() === 'prop');
+            // Find the trait dynamically based on traitType
+            let targetTrait = characterData.attributes.find((attr) => attr.trait_type.toLowerCase() === traitType.toLowerCase());
     
-            if (propTrait) {
-                // Update the trait's value
-                propTrait.value = itemId;
-    
-                // Trigger a re-render
-                setSelectedCharacter({ ...selectedCharacter });
+            // If the trait exists, update its value, else add the trait (optional, based on your use case)
+            if (targetTrait) {
+                targetTrait.value = itemId;
+            } else {
+                // This code will add the trait if it doesn't exist; remove if not needed.
+                targetTrait = { trait_type: traitType, value: itemId };
+                characterData.attributes.push(targetTrait);
             }
+    
+            // Trigger a re-render (assuming selectedCharacter holds the entire character data, adjust accordingly if not)
+            setSelectedCharacter({ ...selectedCharacter });
         }
     };
     
+    const collectionName = CONTRACT_TO_COLLECTION_MAP[selectedCharacter?.contract]
+
+    
     const { data: tokens } = useUserTokens(accountAddress, {
-        collectionsSetId: "2bbf8c77426ecef122b930e50d37eef1eefd8de0eaad268e3ee3abc05d3a2937",
+        collectionsSetId: "a047f7316e9f532792fe87506a904def7a16e09a2c9d09bcbef6a00d86a28135",
     });
     const { data: athenaeumItems } = useUserTokens(accountAddress, {
         collectionsSetId: "6d5746c01bf4b37216420f1f23590386fb69bb1806d38cadcfec37481a14c0d0",
@@ -65,8 +80,6 @@ const Wardrobe2: React.FC = () => {
         return order.map(key => traits[key]);
     }
 
-    
-    
 
     return (
         
@@ -74,17 +87,17 @@ const Wardrobe2: React.FC = () => {
             <h2 className={styles.title}>Choose your Adventurer</h2>
             {equipScreen === EquipScreen.Equip && (
 
-             <div><RenderCharacter traits={characterData.attributes}  />
+             <div><RenderCharacter traits={characterData.attributes} collection={collectionName} />
              <div className="athenaeum-items">
                  {athenaeumItems?.map((item, i) => {
-                     const imagePath = `/assets/athenaeum/${item?.token?.name?.toLowerCase().replace(/\s+/g, '_')}.png`;
+                     const imagePath = `/assets/athenaeum/${item?.token?.name?.toLowerCase().replace(/\s+/g, '_').replace(/'/g, "")}.png`;
                      return (
                          <img
                              key={i}
                              src={imagePath}
                              alt="MISSINGNO"
                              width="25%"
-                             onClick={() => handleEquipItem(item?.token?.name?.toLowerCase().replace(/\s+/g, '_').replace(/'/g, ''))}
+                             onClick={() => handleEquipItem(item?.token?.name?.toLowerCase().replace(/\s+/g, '_').replace(/'/g, ""), 'prop')}
                              className="athenaeum-item"
                          />
                      );
@@ -95,7 +108,7 @@ const Wardrobe2: React.FC = () => {
             {selectedCharacter && (
                 <div>
                     Selected Character ID: {selectedCharacter.id}
-                    Selected Character Contract: {selectedCharacter.contract}
+                    Selected Character Contract: {selectedCharacter.contract} {collectionName}
                     {/* Modified this block to display the character data */}
                     {loading ? (
                         "Loading character data..."
--- components/Wardrobe/characterrender.tsx
+++ components/Wardrobe/characterrender.tsx
@@ -7,39 +7,71 @@ interface Trait {
 
 interface CharacterRenderProps {
     traits: Trait[];
+    collection: string;
 }
 
-const traitOrder = ['background', 'body', 'head', 'familiar', 'prop', 'rune'];
+interface CollectionConfig {
+    singleFolder: boolean;
+    folderName?: string;
+}
+
+
+
+const collectionsConfig: Record<string, CollectionConfig> = {
+    'wizards': { singleFolder: false }, // Existing collection
+    'warriors': { singleFolder: true, folderName: 'warriors' },
+    'babies': { singleFolder: false }, // Existing collection
+
+};
+
+
+const traitOrder = ['background', 'body', 'head', 'familiar', 'companion', 'weapon', 'prop', 'rune'];
 
 const orderTraits = (traits: Trait[]): Trait[] => {
     return traitOrder.map((order) => traits.find((trait) => trait.trait_type.toLowerCase() === order) || { trait_type: order, value: 'default' });
 };
 
-const generateImagePath = (type: string, value: string) => {
-    const formattedValue = value.replace(/\s+/g, '_').replace(/:/g, '').replace(/,/g, '').toLowerCase() + '.png';
-    return `/assets/${type}/${formattedValue}`;
+const generateImagePath = (type: string, value: string, collection: string) => {
+    const config = collectionsConfig[collection];
+    
+    if (!config) {
+        console.error(`No configuration found for collection: ${collection}`);
+        return ''; // Return a default path or handle this case as you see fit.
+    }
+
+    let formattedValue = value.replace(/\s+/g, '_').replace(/'/g, '').toLowerCase() + '.png';
+
+    if (config.singleFolder && config.folderName) {
+        formattedValue = `${type}_${formattedValue}`;
+        return `/assets/${config.folderName}/${formattedValue}`;
+    } else {
+        return `/assets/${type}/${formattedValue}`;
+    }
 };
 
-const CharacterRender: React.FC<CharacterRenderProps> = ({ traits }) => {
-    const orderedTraits = orderTraits(traits);
+
+
+
+const CharacterRender: React.FC<CharacterRenderProps> = ({ traits, collection }) => {
+    console.log("Props in CharacterRender:", { traits, collection });    const orderedTraits = orderTraits(traits);
 
     return (
         <div style={{ width: '500px', height: '500px', position: 'relative' }}>
             {orderedTraits.map((trait, index) => {
     console.log(generateImagePath(trait.trait_type, trait.value));
     return (
-        <img 
-            key={index}
-            src={generateImagePath(trait.trait_type, trait.value)}
-            alt={trait.value}
-            style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%'
-            }}
-        />
+<img 
+    key={index}
+    src={generateImagePath(trait.trait_type, trait.value, collection)} // Pass the collection's name here
+    alt={trait.value}
+    style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%'
+    }}
+/>
     );
 })}
         </div>
